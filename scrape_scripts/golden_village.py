8cc29c5d59f4680ef8bdba04dac0ae31d9094478
Xiongkai <40223039+MAOXIONGKAI@users.noreply.github.com>
2026-07-28 14:49:17 +0800

    feat: verify gv schedule api scraping logic

--- scrape_scripts/golden_village.py
+++ scrape_scripts/golden_village.py
@@ -30,6 +30,28 @@ async def fetch_movie(client: httpx.AsyncClient, movie_id: str):
             print(f"{movie_id}: {e}")
             return None
 
+schedule_info_api = "https://www.gv.com.sg/.gv-api/sessionforfilm"
+async def fetch_schedules(client: httpx.AsyncClient, movie_id: str):
+    async with sem:
+        try:
+            response = await client.post(
+                schedule_info_api,
+                json={"filmCode": movie_id},
+            )
+            response.raise_for_status()
+
+            data = response.json()
+
+            if not data:
+                print(f"{movie_id}: Failed to retrieve schedule info.")
+                return None
+
+            return data
+
+        except Exception as e:
+            print(f"{movie_id} schedule: {e}")
+            return None
+
 async def scrape_golden_village():
     async with async_playwright() as p:
         browser = await p.chromium.launch(
@@ -67,15 +89,26 @@ async def scrape_golden_village():
                 *(fetch_movie(client, movie_id) for movie_id in movie_ids),
                 return_exceptions=True
             )
+
+        async with httpx.AsyncClient(
+            timeout=30,
+            cookies=cookie_dict,
+            headers=headers
+            ) as client:
+            schedules = await asyncio.gather(
+                *(fetch_schedules(client, movie_id) for movie_id in movie_ids),
+                return_exceptions=True
+            )
         
         
         await browser.close()
-        valid = [m for m in movies if isinstance(m, dict)]
+        valid_movies = [m for m in movies if isinstance(m, dict)]
+        valid_schedules = [s for s in schedules if isinstance(s, dict)]
 
         now_ms = int(datetime.now(SG_TZ).timestamp() * 1000)
         coming_soon = []
         showing_now = []
-        for movie in valid:
+        for movie in valid_movies:
             if movie["releaseDate"] > now_ms:
                 coming_soon.append(movie)
             else:
@@ -85,8 +118,11 @@ async def scrape_golden_village():
             f.write(json.dumps(coming_soon, indent=4))
         with open("showing_now.json", "w") as f:
             f.write(json.dumps(showing_now, indent=4))
+        with open("gv_schedules.json", "w") as f:
+            f.write(json.dumps(valid_schedules, indent=4))
 
-        print(f"Scraped {len(valid)} movies successfully!")
+        print(f"Scraped {len(valid_movies)} movies successfully!")
+        print(f"Scraped {len(valid_schedules)} schedules successfully!")
 
 
 if __name__ == "__main__":
