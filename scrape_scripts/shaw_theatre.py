a56f337d74bb03aae65d5e8686cce8aad64215c1
Xiongkai <40223039+MAOXIONGKAI@users.noreply.github.com>
2026-07-28 13:56:23 +0800

    feat: implement shaw theatre scrape script mvp

---
+++ scrape_scripts/shaw_theatre.py
@@ -0,0 +1,95 @@
+import asyncio
+from datetime import datetime, timezone
+import httpx
+import json
+from playwright.async_api import async_playwright
+from zoneinfo import ZoneInfo
+
+SG_TZ = ZoneInfo("Asia/Singapore")
+
+sem = asyncio.Semaphore(5)
+film_info_api = "https://shaw.sg/internal/get_movie_release?id="
+async def fetch_movie(client: httpx.AsyncClient, movie_id: str):
+    async with sem:
+        try:
+            response = await client.get(film_info_api + movie_id)
+            response.raise_for_status()
+
+            data = response.json()
+
+            if not data:
+                print(f"{movie_id}: Failed to retrieve movie info.")
+                return None
+
+            return data
+
+        except Exception as e:
+            print(f"{movie_id}: {e}")
+            return None
+
+
+async def scrape_shaw_theatre():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(
+            headless=False,
+            channel="chrome"
+        )
+        context = await browser.new_context()
+        page = await context.new_page()
+        await page.goto("https://shaw.sg/")
+        movie_ids = await page.locator("a[href^='/movie-details/']").evaluate_all(
+                """
+                    els => [...new Set(els.map(e => e.getAttribute('href').split('/').pop()))]
+                """
+            )
+
+        cookies = await context.cookies()
+
+        cookie_dict = {
+            c["name"]: c["value"]
+            for c in cookies
+        }
+
+        headers = {
+            "User-Agent": await page.evaluate("navigator.userAgent"),
+            "Referer": f"https://shaw.sg/movie-details/",
+            "Accept": "application/json, text/plain, */*",
+            "x-api-forward-to": "internal",
+            "x-app": "PWSM",
+        }
+
+        async with httpx.AsyncClient(
+            timeout=30,
+            headers=headers,
+            cookies=cookie_dict
+        ) as client:
+            movies = await asyncio.gather(
+                *(fetch_movie(client, movie_id) for movie_id in movie_ids),
+                return_exceptions=True
+            )
+        
+        
+        await browser.close()
+        valid = [m for m in movies if isinstance(m, dict)]
+
+        now = datetime.now(SG_TZ)
+        coming_soon = []
+        showing_now = []
+        for movie in valid:
+            release = datetime.fromisoformat(movie["releaseDate"]).replace(tzinfo=SG_TZ)
+            if release > now:
+                coming_soon.append(movie["primaryTitle"])
+            else:
+                showing_now.append(movie["primaryTitle"])
+        
+        with open("coming_soon.json", "w") as f:
+            f.write(json.dumps(coming_soon, indent=4))
+        with open("showing_now.json", "w") as f:
+            f.write(json.dumps(showing_now, indent=4))
+
+        print(f"Scraped {len(valid)} movies successfully!")
+
+if __name__ == "__main__":
+    asyncio.run(scrape_shaw_theatre())
+        
+
