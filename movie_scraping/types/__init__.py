ef3cc1313652d41a3aadbe9594a9cd2e39f2dd6e
Xiongkai <40223039+MAOXIONGKAI@users.noreply.github.com>
2026-08-01 17:40:03 +0800

    fix: import issues

---
+++ movie_scraping/types/__init__.py
@@ -0,0 +1,15 @@
+from .cineams import Cinema, GVCinema, ShawCinema
+from .movies import Movie, GVMovie, ShawMovie
+from .schedules import Schedule, GVShowTime, ShawShowTime
+
+__all__ = [
+    "Cinema",
+    "GVCinema",
+    "ShawCinema",
+    "Movie",
+    "GVMovie",
+    "ShawMovie",
+    "Schedule",
+    "GVShowTime",
+    "ShawShowTime",
+]
